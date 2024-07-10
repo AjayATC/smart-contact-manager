222d3ebf5014b4b8328071642a8e960fa450b4f6
Ajay <ajaythapac@gmail.com>
2024-07-10 15:06:06 +0545

    Add contacts backend

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -3,13 +3,20 @@ package com.smart.controller;
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
+import com.smart.helper.Message;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 @Controller
@@ -53,5 +60,62 @@ public class UserController {
 
         return "normal/add_contact_form";
     }
+
+    //processing add contact form
+    @PostMapping("/process-contact")
+    public String processContact(@ModelAttribute Contact contact,
+                                 @RequestParam("profileImage") MultipartFile file,
+                                 Principal principal,
+                                 HttpSession session) {
+
+        try {
+
+            String name = principal.getName();
+            User user = this.userRepository.getUserByUserName(name);
+
+            //processing and uploading file
+            if (file.isEmpty()) {
+                System.out.println("The file is empty!");
+            } else {
+                //save file to folder and update name to contact
+                contact.setImage(file.getOriginalFilename());
+
+                File saveFile = new ClassPathResource("static/img").getFile();
+                Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
+
+                Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+                System.out.println("Image is uploaded");
+
+            }
+
+            contact.setUser(user);
+            user.getContacts().add(contact);
+
+            this.userRepository.save(user);
+
+            System.out.println("DATA" + contact);
+
+            System.out.println("Added to database");
+
+            //success message
+
+            session.setAttribute("message", new Message("Your contact is added!! Add more..", "success"));
+
+
+        } catch (Exception e) {
+
+            System.out.println("ERROR" + e.getMessage());
+            e.printStackTrace();
+
+            //error message
+
+            session.setAttribute("message", new Message("Something went wrong!! Try again..", "danger"));
+
+        }
+
+        return "normal/add_contact_form";
+    }
+
 }
 
--- src/main/java/com/smart/entities/Contact.java
+++ src/main/java/com/smart/entities/Contact.java
@@ -6,96 +6,96 @@ import javax.persistence.*;
 @Table(name = "CONTACT")
 public class Contact {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int cId;
-	private String name;
-	private String secondName;
-	private String work;
-	private String email;
-	private String phone;
-	private String image;
-	@Column(length = 1000)
-	private String description;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int cId;
+    private String name;
+    private String secondName;
+    private String work;
+    private String email;
+    private String phone;
+    private String image;
+    @Column(length = 1000)
+    private String description;
 
-	@ManyToOne
-	private User user;
+    @ManyToOne
+    private User user;
 
-	public Contact() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    public Contact() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-	public int getcId() {
-		return cId;
-	}
+    public int getcId() {
+        return cId;
+    }
 
-	public void setcId(int cId) {
-		this.cId = cId;
-	}
+    public void setcId(int cId) {
+        this.cId = cId;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getSecondName() {
-		return secondName;
-	}
+    public String getSecondName() {
+        return secondName;
+    }
 
-	public void setSecondName(String secondName) {
-		this.secondName = secondName;
-	}
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
 
-	public String getWork() {
-		return work;
-	}
+    public String getWork() {
+        return work;
+    }
 
-	public void setWork(String work) {
-		this.work = work;
-	}
+    public void setWork(String work) {
+        this.work = work;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public String getPhone() {
-		return phone;
-	}
+    public String getPhone() {
+        return phone;
+    }
 
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
-	public String getImage() {
-		return image;
-	}
+    public String getImage() {
+        return image;
+    }
 
-	public void setImage(String image) {
-		this.image = image;
-	}
+    public void setImage(String image) {
+        this.image = image;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public User getUser() {
-		return user;
-	}
+    public User getUser() {
+        return user;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
-
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
 }
